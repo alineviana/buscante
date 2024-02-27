152951456b3b36e44442b1262349685cbc54c359
Aline Viana <80078418+alineviana@users.noreply.github.com>
2024-02-27 14:16:29 -0300

    add files

---
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from 'src/app/service/livro.service';
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent implements OnDestroy {
+
+  listaLivros: [];
+  campoBusca: string = '';
+  subscription: Subscription;
+
+  constructor(private service: LivroService) { }
+
+  buscarLivros() {
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => console.log(retornoAPI),
+      error: erro => console.error(erro),
+      complete: () => console.log('Observable completado!')
+    })
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+}
